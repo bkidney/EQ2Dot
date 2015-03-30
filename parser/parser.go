3b0bcf23c6e998a4a3307b7e63c67b78c9b5604e
Brian Kidney <bkidney@briankidney.ca>
2015-03-30 20:55:42 -0230

    Reformats code with gofmt.

--- parser/parser.go
+++ parser/parser.go
@@ -1,157 +1,157 @@
 package parser
 
 import (
-  "io"
-  "fmt"
-  "github.com/bkidney/gofelex"
+	"fmt"
+	"github.com/bkidney/gofelex"
+	"io"
 )
 
 type Parser struct {
-  s *gofelex.Scanner
-  buf struct {
-    tok gofelex.Token // Last read token
-    lit string // Last read literal
-    n int // Buffer Size (max = 1)
-  }
+	s   *gofelex.Scanner
+	buf struct {
+		tok gofelex.Token // Last read token
+		lit string        // Last read literal
+		n   int           // Buffer Size (max = 1)
+	}
 }
 
 func NewParser(r io.Reader) *Parser {
-  return &Parser{s: gofelex.NewScanner(r)}
+	return &Parser{s: gofelex.NewScanner(r)}
 }
 
 func (p *Parser) Parse() (string, error) {
-  p.scanIgnoreWhitespace()
-  return p.query()
+	p.scanIgnoreWhitespace()
+	return p.query()
 }
 
 func (p *Parser) query() (string, error) {
-  var out, ret string
-  var err error
+	var out, ret string
+	var err error
 
-  err = nil
+	err = nil
 
-  if p.buf.tok == gofelex.IDENT {
-    out, err = p.action()
-  } else {
-    err = fmt.Errorf("found %q, expected IDENT", p.buf.lit)
-    return "", err
-  }
+	if p.buf.tok == gofelex.IDENT {
+		out, err = p.action()
+	} else {
+		err = fmt.Errorf("found %q, expected IDENT", p.buf.lit)
+		return "", err
+	}
 
-  ret = out
+	ret = out
 
-  if p.buf.tok != gofelex.EOF {
-    out, err = p.join()
-    ret = ret + " " + out
-  }
+	if p.buf.tok != gofelex.EOF {
+		out, err = p.join()
+		ret = ret + " " + out
+	}
 
-  return ret, err 
+	return ret, err
 }
 
 func (p *Parser) action() (string, error) {
-  var out string
-  var err error
+	var out string
+	var err error
 
-  out = "IDENT"
-  err = nil
+	out = "IDENT"
+	err = nil
 
-  p.scanIgnoreWhitespace()
-  return out, err
+	p.scanIgnoreWhitespace()
+	return out, err
 }
 
 func (p *Parser) join() (string, error) {
-  var ret, out string
-  var err error
+	var ret, out string
+	var err error
 
-  if p.buf.tok == gofelex.LOGICAL {
-    out, err = p.logical()
-  } else if p.buf.tok == gofelex.TEMPORAL {
-    out, err = p.temporal()
-  } else if p.buf.tok == gofelex.CONDITION {
-    out, err = p.conditional()
-  } else if p.buf.tok == gofelex.FLOW {
-    out, err = p.flow()
-  } else {
-    err = fmt.Errorf("found %q, expected join type", p.buf.lit)
-    return "", err
-  }
+	if p.buf.tok == gofelex.LOGICAL {
+		out, err = p.logical()
+	} else if p.buf.tok == gofelex.TEMPORAL {
+		out, err = p.temporal()
+	} else if p.buf.tok == gofelex.CONDITION {
+		out, err = p.conditional()
+	} else if p.buf.tok == gofelex.FLOW {
+		out, err = p.flow()
+	} else {
+		err = fmt.Errorf("found %q, expected join type", p.buf.lit)
+		return "", err
+	}
 
-  ret = out
-  out, err = p.query()
+	ret = out
+	out, err = p.query()
 
-  ret = ret + " " + out
+	ret = ret + " " + out
 
-  return ret, err
+	return ret, err
 }
 
 func (p *Parser) logical() (string, error) {
-  var out string
-  var err error
+	var out string
+	var err error
 
-  out = "LOGICAL"
-  err = nil
+	out = "LOGICAL"
+	err = nil
 
-  p.scanIgnoreWhitespace()
-  return out, err
+	p.scanIgnoreWhitespace()
+	return out, err
 }
 
 func (p *Parser) temporal() (string, error) {
-  var out string
-  var err error
+	var out string
+	var err error
 
-  out = "TEMPORAL"
-  err = nil
+	out = "TEMPORAL"
+	err = nil
 
-  p.scanIgnoreWhitespace()
-  return out, err
+	p.scanIgnoreWhitespace()
+	return out, err
 }
 
 func (p *Parser) conditional() (string, error) {
-  var out string
-  var err error
+	var out string
+	var err error
 
-  out = "CONDITION"
-  err = nil
+	out = "CONDITION"
+	err = nil
 
-  p.scanIgnoreWhitespace()
-  return out, err
+	p.scanIgnoreWhitespace()
+	return out, err
 }
 
 func (p *Parser) flow() (string, error) {
-  var out string
-  var err error
+	var out string
+	var err error
 
-  out = "FLOW"
-  err = nil
+	out = "FLOW"
+	err = nil
 
-  p.scanIgnoreWhitespace()
-  return out, err
+	p.scanIgnoreWhitespace()
+	return out, err
 }
 
 // Utility Functions
 func (p *Parser) scan() (tok gofelex.Token, lit string) {
 
-  // Return any token in the buffer
-  if p.buf.n != 0 {
-    p.buf.n = 0
-    return p.buf.tok, p.buf.lit
-  }
+	// Return any token in the buffer
+	if p.buf.n != 0 {
+		p.buf.n = 0
+		return p.buf.tok, p.buf.lit
+	}
 
-  // Otherwise read a new one in
-  tok, lit = p.s.Scan()
+	// Otherwise read a new one in
+	tok, lit = p.s.Scan()
 
-  // And save to buffer
-  p.buf.tok, p.buf.lit = tok, lit
+	// And save to buffer
+	p.buf.tok, p.buf.lit = tok, lit
 
-  return
+	return
 }
 
 func (p *Parser) scanIgnoreWhitespace() {
-  var tok gofelex.Token
+	var tok gofelex.Token
 
-  tok, _ = p.scan()
-  if tok == gofelex.WS {
-    tok, _ = p.scan()
-  }
+	tok, _ = p.scan()
+	if tok == gofelex.WS {
+		tok, _ = p.scan()
+	}
 }
 
 func (p *Parser) unscan() { p.buf.n = 1 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,62 +1,62 @@
 package parser_test
 
 import (
-  "strings"
-  "testing"
-  "reflect"
+	"reflect"
+	"strings"
+	"testing"
 
-  "github.com/bkidney/EQ2Dot/parser"
+	"github.com/bkidney/EQ2Dot/parser"
 )
 
 func TestParser_ParseQuery(t *testing.T) {
-  var tests = []struct {
-    s string
-    out string
-    err string
-  }{
-    // Simple pattern match 
-    {
-      s: `?node:ip:send(?srcIP,?destIP) and destIP in [203.0.113.12,192.168.1.100]`,
-      out: `IDENT LOGICAL IDENT CONDITION IDENT`,
-    },
-    // A more complete example
-    {
-      s: `
- DBServerNode:myDB:openSession(_):?sessionID Within
+	var tests = []struct {
+		s   string
+		out string
+		err string
+	}{
+		// Simple pattern match
+		{
+			s:   `?node:ip:send(?srcIP,?destIP) and destIP in [203.0.113.12,192.168.1.100]`,
+			out: `IDENT LOGICAL IDENT CONDITION IDENT`,
+		},
+		// A more complete example
+		{
+			s: `
+DBServerNode:myDB:openSession(_):?sessionID Within
     DBServerNode:myDb:userAuthenticate:(?user) Precedes
         DBServerNode:myDB:sqlQuery(sessionID):?resultData Precedes
           ?egressNode:ip::send(?outData,203.0.113.12)
 and resultData FlowsTo* outData
       `,
-      out: `IDENT TEMPORAL IDENT TEMPORAL IDENT TEMPORAL IDENT LOGICAL IDENT FLOW IDENT`,
-    },
-    // Errors
-    {
-      s: `and ?srcIP`,
-      out: ``,
-      err: `found "and", expected IDENT`,
-    },
-    {
-      s: `?node:ip:send(?srcIP,?destIP) and in [203.0.113.12,192.168.1.100]`,
-      out: ``,
-      err: `found "in", expected IDENT`,
-    },
-  }
+			out: `IDENT TEMPORAL IDENT TEMPORAL IDENT TEMPORAL IDENT LOGICAL IDENT FLOW IDENT`,
+		},
+		// Errors
+		{
+			s:   `and ?srcIP`,
+			out: ``,
+			err: `found "and", expected IDENT`,
+		},
+		{
+			s:   `?node:ip:send(?srcIP,?destIP) and in [203.0.113.12,192.168.1.100]`,
+			out: ``,
+			err: `found "in", expected IDENT`,
+		},
+	}
 
-  for i, tt := range tests {
-    out, err := parser.NewParser(strings.NewReader(tt.s)).Parse()
-    if !reflect.DeepEqual(tt.err, errstring(err)) {
-      t.Errorf("%d. %q: error mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, tt.err, err)
-    } else if tt.err == "" && !reflect.DeepEqual(tt.out, out) {
-      t.Errorf("%d. %q\noutput mismatch:\n\nexp%#v\n\ngot=%#v\n\n", i, tt.s, tt.out, out)
-    }
-  }
+	for i, tt := range tests {
+		out, err := parser.NewParser(strings.NewReader(tt.s)).Parse()
+		if !reflect.DeepEqual(tt.err, errstring(err)) {
+			t.Errorf("%d. %q: error mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, tt.err, err)
+		} else if tt.err == "" && !reflect.DeepEqual(tt.out, out) {
+			t.Errorf("%d. %q\noutput mismatch:\n\nexp%#v\n\ngot=%#v\n\n", i, tt.s, tt.out, out)
+		}
+	}
 }
 
 // errstring returns the string representation of an error
-func errstring (err error) string {
-  if err != nil {
-    return err.Error()
-  }
-  return ""
-}
+func errstring(err error) string {
+	if err != nil {
+		return err.Error()
+	}
+	return ""
+,
