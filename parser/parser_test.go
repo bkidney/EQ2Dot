21c3d1390bed80dbb71777e1800cd8826786251b
Brian Kidney <bkidney@briankidney.ca>
2015-03-30 14:08:28 -0230

    Adds testing for TOKENIZE output to test parsing.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,26 +11,33 @@ import (
 func TestParser_ParseQuery(t *testing.T) {
   var tests = []struct {
     s string
+    out string
     err string
   }{
     // Simple pattern match 
     {
       s: `?node:ip:send(?srcIP,?destIP) and destIP in [203.0.113.12,192.168.1.100]`,
+      out: `IDENT LOGICAL IDENT CONDITION IDENT`,
     },
     // Errors
     {
       s: `and ?srcIP`,
+      out: ``,
       err: `found "and", expected IDENT`,
     },
     {
       s: `?node:ip:send(?srcIP,?destIP) and in [203.0.113.12,192.168.1.100]`,
+      out: ``,
+      err: `found "in", expected IDENT`,
     },
   }
 
   for i, tt := range tests {
-    _, err := parser.NewParser(strings.NewReader(tt.s)).Parse()
+    out, err := parser.NewParser(strings.NewReader(tt.s)).Parse()
     if !reflect.DeepEqual(tt.err, errstring(err)) {
       t.Errorf("%d. %q: error mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, tt.err, err)
+    } else if tt.err == "" && !reflect.DeepEqual(tt.out, out) {
+      t.Errorf("%d. %q\noutput mismatch:\n\nexp%#v\n\ngot=%#v\n\n", i, tt.s, tt.out, out)
     }
   }
 }
