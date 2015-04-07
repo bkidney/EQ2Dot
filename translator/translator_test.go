3c1a14beda6af368f263ef2615689b43f7afc5e9
Brian Kidney <bkidney@briankidney.ca>
2015-04-07 13:29:10 -0230

    Adds simple test case using or.

--- translator/translator_test.go
+++ translator/translator_test.go
@@ -19,7 +19,7 @@ func TestTranlator_ProduceDot(t *testing.T) {
 		// Simple pattern match
 		{
 			s:   `?node:ip:send(?srcIP,?destIP) and destIP in [203.0.113.12,192.168.1.100]`,
-			out: "digraph query {\n\tnode [shape = none];\n\t0 [label = \"start\"];\n\tnode [shape = circle];\n\t0 -> 1;\n\t1 -> 2 [label = \"?node:ip:send(?srcIP,?destIP)\"];\n\tnode [shape = doublecircle];\n\t2 -> 3 [label = \"destIP in [203.0.113.12,192.168.1.100]\"];\n}",
+			out: "digraph query {\n\tnode [shape = none];\n\t0 [label = \"start\"];\n\tnode [shape = circle];\n\t0 -> 1;\n\t1 -> 2 [label = \"?node:ip:send(?srcIP,?destIP)\"];\n\t2 -> 3 [label = \"destIP in [203.0.113.12,192.168.1.100]\"];\n\t3 [shape = doublecircle];\n}",
 		},
 		// A more complete example
 		{
@@ -30,7 +30,12 @@ func TestTranlator_ProduceDot(t *testing.T) {
 			  ?egressNode:ip::send(?outData,203.0.113.12)
 		 and resultData FlowsTo* outData
 		 `,
-			out: "digraph query {\n\tnode [shape = none];\n\t0 [label = \"start\"];\n\tnode [shape = circle];\n\t0 -> 1;\n\t1 -> 2 [label = \"Call - DBServerNode:myDB:openSession(_):?sessionID\"];\n\tsubgraph cluster_0 {\n\trank = same;\n\tstyle=\"dashed\";\n\t2 -> 3 [label = \"DBServerNode:myDb:userAuthenticate:(?user)\"];\n\t}\t3 -> 4 [label = \"Ret - DBServerNode:myDB:openSession(_):?sessionID\"];\n\t4 -> 5 [label = \"DBServerNode:myDB:sqlQuery(sessionID):?resultData\"];\n\t5 -> 6 [label = \"?egressNode:ip::send(?outData,203.0.113.12)\"];\n\tnode [shape = doublecircle];\n\t6 -> 7 [label = \"resultData FlowsTo* outData\"];\n}",
+			out: "digraph query {\n\tnode [shape = none];\n\t0 [label = \"start\"];\n\tnode [shape = circle];\n\t0 -> 1;\n\t1 -> 2 [label = \"Call - DBServerNode:myDB:openSession(_):?sessionID\"];\n\tsubgraph cluster_0 {\n\trank = same;\n\tstyle=\"dashed\";\n\t2 -> 3 [label = \"DBServerNode:myDb:userAuthenticate:(?user)\"];\n\t}\t3 -> 4 [label = \"Ret - DBServerNode:myDB:openSession(_):?sessionID\"];\n\t4 -> 5 [label = \"DBServerNode:myDB:sqlQuery(sessionID):?resultData\"];\n\t5 -> 6 [label = \"?egressNode:ip::send(?outData,203.0.113.12)\"];\n\t6 -> 7 [label = \"resultData FlowsTo* outData\"];\n\t7 [shape = doublecircle];\n}",
+		},
+		// A simple example using 'or' with simplified identifiers.
+		{
+			s:   `a or {b and c}`,
+			out: "digraph query {\n\tnode [shape = none];\n\t0 [label = \"start\"];\n\tnode [shape = circle];\n\t0 -> 1;\n\t1 -> 3 [label = \"a\"];\n\t1 -> 2 [label = \"b\"];\n\t2 -> 3 [label = \"c\"];\n\t3 [shape = doublecircle];\n}",
 		},
 	}
 
